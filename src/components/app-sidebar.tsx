49d0d4fdcee23c7b0fc6789e214024f189371245
Bayuara <bayuara168@gmail.com>
2025-08-10 00:52:17 +0700

    feat: sidebar and routing use tanstack

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,67 @@
+import { Banknote, Home, CreditCard, History, Settings } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Account",
+    url: "/account",
+    icon: CreditCard,
+  },
+  {
+    title: "Transactions",
+    url: "transaction",
+    icon: Banknote,
+  },
+  {
+    title: "Transaction History",
+    url: "history",
+    icon: History,
+  },
+  {
+    title: "Settings",
+    url: "setting",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Keuangan</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
